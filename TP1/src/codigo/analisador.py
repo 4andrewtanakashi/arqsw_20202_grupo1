cb153e8b2024f6f85d3853b3b91ff8029145415e
ghamorim <gabriel.amorim@estudante.ufla.br>
2020-11-30 00:10:43 -0300

    :hammer: Retira codigo desnecessário

--- TP1/src/codigo/analisador.py
+++ TP1/src/codigo/analisador.py
@@ -15,56 +15,6 @@ matrix_inherit = {}
 classes_methods = {}
 
 
-class Python3ErrorListener(ErrorListener):
-    def __init__(self, output):
-        self.output = output
-        self._symbol = ''
-
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        self.output.write(msg)
-        self._symbol = offendingSymbol.text
-        stack = recognizer.getRuleInvocationStack()
-        stack.reverse()
-        print("rule stack: {}".format(str(stack)))
-        print("line {} : {} at {} : {}".format(str(line),
-                                               str(column),
-                                               str(offendingSymbol).replace(" ", u'\u23B5'),
-                                               msg.replace(" ", u'\u23B5')))
-
-    @property
-    def symbol(self):
-        return self._symbol
-
-    
-def setup(path):
-    input = FileStream(path)
-    lexer = Python3Lexer(input)
-    stream = CommonTokenStream(lexer)
-
-    stream.fill()
-
-    # print out the token parsing
-    # print("TOKENS")
-    # for token in stream.tokens:
-    #     if token.text != '<EOF>':
-    #         type_name = Python3Parser.symbolicNames[token.type]
-    #         tabs = 5 - len(type_name) // 4
-    #         sep = "\t" * tabs
-    #         print("    %s%s%s" % (type_name, sep,
-    #                               token.text.replace(" ", u'\u23B5').replace("\n", u'\u2936')))
-
-    parser = Python3Parser(stream)
-
-    output = io.StringIO()
-    error = io.StringIO()
-
-    parser.removeErrorListeners()
-    errorListener = Python3ErrorListener(error)
-    parser.addErrorListener(errorListener)
-
-    return parser
-
-
 class RuleListener(Python3Listener):
     def enterEveryRule(self, ctx):
         global matrix_inherit
@@ -117,7 +67,13 @@ if __name__ == '__main__':
 
     try:
         for path in files_path:
-            parser = setup(path)
+            input = FileStream(path)
+            lexer = Python3Lexer(input)
+            stream = CommonTokenStream(lexer)
+
+            stream.fill()
+
+            parser = Python3Parser(stream)
             tree = parser.file_input()
             listener = RuleListener()
             walker = ParseTreeWalker()
@@ -134,10 +90,8 @@ if __name__ == '__main__':
                 for i in range(len(parents_class) - 1):
                     for j in range(i+1, len(parents_class)):
                         ambig_methods = list(set(classes_methods[parents_class[i]]).intersection(classes_methods[parents_class[j]]))
-                        #print('Metodos ambiguos:', metodos_ambiguos)
                         if len(ambig_methods) > 0:
                             for metodo in ambig_methods:
-                                #print('Classes heranca ambigua:', classes_heranca_ambigua)
                                 if metodo in ambig_inherit_classes.keys():
                                     if parents_class[i] not in ambig_inherit_classes[metodo]:
                                         ambig_inherit_classes[metodo].append(parents_class[i])
